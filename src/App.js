e9b1e8fac993bb717031011c2047179769fb9e0b
Yordanov <iliyanyyordanov@gmail.com>
2020-10-30 15:54:51 +0000

    Redux store integration with custom environment variables
    Signed-off-by: Yordanov <iliyanyyordanov@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import configureStore from "./store/configureStore";
+import { Provider } from "react-redux";
+import { loadUsers } from './store/users';
+import { loadCustomers } from './store/customers';
+
+const store = configureStore();
+
 
 function App() {
+
+  useEffect(()=>{
+    store.dispatch(loadUsers());
+    store.dispatch(loadCustomers());
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <React.Fragment>
+        
+      </React.Fragment>
+    </Provider>
   );
 }
 
